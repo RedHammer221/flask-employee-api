d1e85f658990cee6e31625e8f448e24cbcd4860e
RedHammer221 <43749545+RedHammer221@users.noreply.github.com>
2026-05-09 23:46:16 +0300

    Created endpoint to get all employess

--- backend/app/routes/employee_routes.py
+++ backend/app/routes/employee_routes.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from app import db
 from app.models.employee import Employee
 
 employee_bp = Blueprint('employee_bp', __name__)
 
 
-@employee_bp.route('/employees')
+# GET ALL EMPLOYEES
+@employee_bp.route('/employees', methods=['GET'])
 def get_employees():
+
     employees = Employee.query.all()
 
     result = []
@@ -15,7 +19,30 @@ def get_employees():
             "id": employee.id,
             "first_name": employee.first_name,
             "last_name": employee.last_name,
-            "email": employee.email
+            "email": employee.email,
+            "department": employee.department
         })
 
     return jsonify(result)
+
+
+# CREATE EMPLOYEE
+@employee_bp.route('/employees', methods=['POST'])
+def create_employee():
+
+    data = request.get_json()
+
+    new_employee = Employee(
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        email=data['email'],
+        department=data['department']
+    )
+
+    db.session.add(new_employee)
+
+    db.session.commit()
+
+    return jsonify({
+        "message": "Employee created successfully"
+    }), 201
